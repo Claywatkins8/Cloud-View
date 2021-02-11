f1062a12d5c5e7b661882ea5f598f7aa4a3018db
claywatkins8 <claywatkins8@gmail.com>
2021-02-11 13:17:53 -0800

    adds heroku db

--- cloud_view_app/settings.py
+++ cloud_view_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'i-b6o@uc&8@dyfot%rasvhsn7v==4mz91ve2a$rmg*q!=y3n99'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
