0ab8802ed81a0e2ae6345f43a2105af1f945f7a5
claywatkins8 <claywatkins8@gmail.com>
2021-02-17 15:14:17 -0800

    update secret key

--- cloud_view_app/settings.py
+++ cloud_view_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i-b6o@uc&8@dyfot%rasvhsn7v==4mz91ve2a$rmg*q!=y3n99'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
